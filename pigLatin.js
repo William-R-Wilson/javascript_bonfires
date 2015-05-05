0ac23964afb2af516b1611c133789d54a278e56d
Will <williamrwilson@gmail.com>
2015-05-05 01:11:35 +0000

    finish 22

--- pigLatin.js
+++ pigLatin.js
@@ -1,10 +1,25 @@
 function translate(str) {
   var wordLength = str.length - 1; 
-  if (str.charAt(0) === "e") { //can I use an array of vowels to compare to here?
+  var vowels = ["a","e","i","o","u"];
+  var dipthongs = ["gl"];
+  for (var i = 0; i < vowels.length; i++) {
+      if (str.charAt(0) === vowels[i]) { 
     return str + "way";
+    }
   }
-  var translated = str.substr(1, wordLength);
-  return translated+str.charAt(0)+"ay";
+  for (var x = 0; x < dipthongs.length; x++) {
+      if (str.substr(0,2) === dipthongs[x]) {
+        
+          var translated = str.substr(2, wordLength);
+          var initial = str.substr(0,2);
+          return translated + initial + "ay";
+      }
+  }
+    var translated = str.substr(1, wordLength);
+    return translated+str.charAt(0)+"ay";
+  
 }
 
-translate("consonant");
+translate("glove");
+
+
